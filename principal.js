03c2b817b0f02b54441f10bbb853d31b9d6c432d
Renan Jansen Lopes de Faria <66344616+renanjansen@users.noreply.github.com>
2020-08-10 23:44:28 -0300

    ajustte de fórmula

--- principal.js
+++ principal.js
@@ -2,6 +2,6 @@ function calculaIc(){
 var peso = document.ic.peso.value;
 var altura = document.ic.altura.value;
 var cintura  = document.ic.cintura.value;
-var ic = cintura /(0.109*(peso/altura));
+var ic = (cintura/100) /(0.109 * Math.sqrt((peso/altura)));
 alert(ic);
 }
