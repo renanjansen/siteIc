1366ca16a52fe06cd14b82ee40bc61992a6e12cb
Renan Jansen Lopes de Faria <66344616+renanjansen@users.noreply.github.com>
2020-08-12 18:28:56 -0300

    estou estudando um meio de colocar a variável sexo

--- principal.js
+++ principal.js
@@ -1,7 +1,11 @@
 function calculaIc(){
+//var sexo = document.ic.genero.value;
 var peso = document.ic.peso.value;
 var altura = document.ic.altura.value;
 var cintura  = document.ic.cintura.value;
 var ic = (cintura/100) /(0.109 * Math.sqrt((peso/altura)));
-alert(ic);
+    
+    document.getElementById('resultado').innerHTML = `Seu resultado foi `+ic.toFixed(2) ;
+
+
 }
