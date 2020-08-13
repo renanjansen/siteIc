6cfc443005094afe8a3adb10f35e635b406f57c0
Renan Jansen Lopes de Faria <66344616+renanjansen@users.noreply.github.com>
2020-08-13 19:02:20 -0300

    finaliação

--- principal.js
+++ principal.js
@@ -4,8 +4,25 @@ var peso = document.ic.peso.value;
 var altura = document.ic.altura.value;
 var cintura  = document.ic.cintura.value;
 var ic = (cintura/100) /(0.109 * Math.sqrt((peso/altura)));
+if(sexo = "Homem" && ic >= 1.25 ){
     
-    document.getElementById('resultado').innerHTML = `Seu resultado foi `+ic.toFixed(2) ;
+    document.getElementById('resultado').innerHTML = `Seu resultado foi `+ic.toFixed(2)+'você possui alto risco de doenças coronarianas' ;
+}
 
-    alert(sexo)//chamei o alerte para testar a variavel sexo
+else {
+    document.getElementById('resultado').innerHTML = `Seu resultado foi `+ic.toFixed(2)+'você  não possui alto risco de doenças coronarianas';
+}
+
+if(sexo ="Mulher" && ic >= 1.18){
+
+    document.getElementById('resultado').innerHTML = `Seu resultado foi `+ic.toFixed(2)+'você possui alto risco de doenças coronarianas' ;
+
+}
+else{
+
+    document.getElementById('resultado').innerHTML = `Seu resultado foi `+ic.toFixed(2)+' você  não possui alto risco de doenças coronarianas' ;
+
+
+}
+    
 }
